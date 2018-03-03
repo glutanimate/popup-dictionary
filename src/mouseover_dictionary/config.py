1e6f2591f31c1c7aafa81e53c4623e651d574658
Glutanimate <glutanimate@users.noreply.github.com>
2018-03-03 12:30:20 +0100

    Implement note snippet lookup

--- src/mouseover_dictionary/config.py
+++ src/mouseover_dictionary/config.py
@@ -13,131 +13,61 @@ from __future__ import unicode_literals
 
 ############## USER CONFIGURATION START ##############
 
+# GENERAL
+
+# Mode selection
+MODE = "snippets"  # dictionary/snippets
+# Show tooltip even if no results found?
+ALWAYS_SHOW = False
+
+# DICTIONARY MODE
 DECK = "Dictionary"
 NOTETYPE = "Dictionary Entry"
 TERM_FIELD = "Term"
 DEFINITION_FIELD = "Definition"
+
+# SNIPPET MODE
+EXCLUDED_FIELDS = ["Extra"]
+
+# TOOLTIP STYLING
 USER_STYLES = r"""
 .qtip {
-    font-family: "arial";
+    font-family: "arial", sans-serif;
     max-width: 400px;
     max-height: 400px;
     overflow-y: auto;
+    color: #141414;
+    background-color: white;  
+}
+.night_mode > .qtip{
+    background-color: #141414;
+    color: white;  
+}
+.qtip-bootstrap .qtip-content {
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+    padding-top: 0.2em;
+    padding-bottom: 0.2em;
+}
+
+.tt-res {
+    margin-top: 0.5em;
+    margin-bottom: 0.5em;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+    border-style: solid;
+    border-width: 0.1em;
+    border-color: #7E7680;
+    border-radius: 0.1em;
+}
+.tt-fld {
+    margin-top: 0.5em;
+}
+.tt-reslist {
+    text-align: center;
 }
 """
 
 ##############  USER CONFIGURATION END  ##############
-
-import os
-import io
-
-from aqt import mw
-from anki.utils import json
-
-from .consts import *
-
-defaults_path = os.path.join(addon_path, "config.json")
-meta_path = os.path.join(addon_path, "meta.json")
-
-if anki21:
-    def getConfig():
-        return mw.addonManager.getConfig(__name__)
-
-    def writeConfig(config):
-        mw.addonManager.writeConfig(__name__, config)
-
-else:
-    def _addonMeta():
-        """Get meta dictionary
-
-        Reads in meta.json in add-on folder and returns
-        resulting dictionary of user-defined metadata values.
-
-        Note:
-            Anki 2.1 stores both add-on meta data and customized
-            settings in meta.json. In this module we are only dealing
-            with the settings part.
-
-        Returns:
-            dict: config dictionary
-
-        """
-
-        try:
-            meta = json.load(io.open(meta_path, encoding="utf-8"))
-        except (IOError, OSError, json.decoder.JSONDecodeError):
-            meta = None
-
-        if not meta:
-            meta = {"config": _addonConfigDefaults()}
-            _writeAddonMeta(meta)
-
-        return meta
-
-    def _writeAddonMeta(meta):
-        """Write meta dictionary
-
-        Writes meta dictionary to meta.json in add-on folder.
-
-        Args:
-            meta (dict): meta dictionary
-
-        """
-
-        with io.open(meta_path, 'w', encoding="utf-8") as f:
-            f.write(unicode(json.dumps(meta, indent=4,
-                                       sort_keys=True,
-                                       ensure_ascii=False)))
-
-    def _addonConfigDefaults():
-        """Get default config dictionary
-
-        Reads in config.json in add-on folder and returns
-        resulting dictionary of default config values.
-
-        Returns:
-            dict: config dictionary
-
-        Raises:
-            Exception: If config.json cannot be parsed correctly.
-                (The assumption being that we would end up in an
-                inconsistent state if we were to return an empty
-                config dictionary. This should never happen.)
-
-        """
-
-        try:
-            return json.load(io.open(defaults_path, encoding="utf-8"))
-        except (IOError, OSError, json.decoder.JSONDecodeError) as e:
-            raise Exception("External Editor for Text Files – "
-                            "Config file could not be read: " + str(e))
-
-    def getConfig():
-        """Get user config dictionary
-
-        Merges user's keys into default config dictionary
-        and returns the result.
-
-        Returns:
-            dict: config dictionary
-
-        """
-
-        config = _addonConfigDefaults()
-        meta = _addonMeta()
-        userConf = meta.get("config", {})
-        config.update(userConf)
-        return config
-
-    def writeConfig(config):
-        """Write user config dictionary
-
-        Saves user's config dictionary via meta.json.
-
-        Args:
-            config (dict): user config dictionary
-
-        """
-
-        _writeAddonMeta({"config": config})
-
--- src/mouseover_dictionary/main.py
+++ src/mouseover_dictionary/main.py
@@ -11,6 +11,8 @@ License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
 
 from __future__ import unicode_literals
 
+import re
+
 from aqt.qt import *
 from aqt import mw
 from aqt.reviewer import Reviewer
@@ -18,10 +20,18 @@ from anki.hooks import wrap, addHook
 
 from .js import html
 from .consts import *
-from .config import (DECK, NOTETYPE, TERM_FIELD,
-                     DEFINITION_FIELD, USER_STYLES)
+from .config import (MODE, DECK, NOTETYPE, TERM_FIELD,
+                     DEFINITION_FIELD, USER_STYLES,
+                     EXCLUDED_FIELDS, ALWAYS_SHOW)
 from .template import addModel
 
+
+# support for JS Booster add-on
+try:
+    from jsbooster import review_hack
+    JSBOOSTER = True
+except ImportError:
+    JSBOOSTER = False
 
 class DictionaryLookup(QObject):
     """
@@ -39,13 +49,56 @@ class DictionaryLookup(QObject):
         return self.generateDefinition(term)
 
     def generateDefinition(self, term):
-        return searchDefinitionFor(term.strip())
+        return getNoteSnippetsFor(term.strip())
 
 
 dictLookup = DictionaryLookup()
 
 
+html_reslist = """<div class="tt-reslist">{}</div>"""
+html_snippet = """<div class="tt-res">{}</div>"""
+html_field = """<div class="tt-fld">{}</div>"""
+
+cloze_re_str = r"\{\{c(\d+)::(.*?)(::(.*?))?\}\}"
+cloze_re = re.compile(cloze_re_str)
+
+def getNoteSnippetsFor(term):
+    """Find relevant note snippets for search term"""
+    print("getNoteSnippetsFor")
+    # exclude current note
+    current_nid = mw.reviewer.card.note().id
+    exclusion_string = "-nid:{} ".format(current_nid)
+    # exclude matches in predefined fields
+    # exclusion_string += " ".join(['''-"{}:*{}*"'''.format(fld, term)
+    #                             for fld in EXCLUDED_FIELDS])
+    # construct query string
+    query = u'''deck:current "{}" {}'''.format(term, exclusion_string)
+
+    # NOTE: performing the SQL query directly might be faster
+    res = sorted(mw.col.findNotes(query))  
+    if not res:
+        if ALWAYS_SHOW:
+            return "No results found."
+        else:
+            return ""
+    
+    print("Query finished.")
+
+    content = []
+    for nid in res:
+        note = mw.col.getNote(nid)
+        valid_flds = [html_field.format(i[1]) for i in note.items() if i[0] not in EXCLUDED_FIELDS]
+        joined_flds = "".join(valid_flds)
+        filtered_flds = cloze_re.sub(r"\2", joined_flds)
+        content.append(html_snippet.format(filtered_flds))
+    
+    html = html_reslist.format("".join(content))
+
+    print("Html compiled")
+    return html
+
 def searchDefinitionFor(term):
+    """Look up search term in dictionary deck"""
     query = u"""note:"{}" {}:"{}" """.format(NOTETYPE, TERM_FIELD, term)
     res = mw.col.findNotes(query)
     if res:
@@ -56,24 +109,31 @@ def searchDefinitionFor(term):
 
 
 def addJavascriptObjects(self):
-    # add the callback object
+    """Add python object to JS"""
     self.web.page().mainFrame().addToJavaScriptWindowObject("pyDictLookup", dictLookup)
 
 
 def setupAddon():
-    """Prepare note type and deck"""
-    did = mw.col.decks.byName(DECK)
-    if not did:
-        mw.col.decks.id(DECK)
-    mid = mw.col.models.byName(NOTETYPE)
-    if not mid:
-        addModel(mw.col)
-    if not did or not mid:
-        mw.reset()
+    """Setup hooks, prepare note type and deck"""
+    Reviewer._initWeb = wrap(Reviewer._initWeb, addJavascriptObjects, "after")
+    # JSBooster support:
+    if not JSBOOSTER:
+        Reviewer._revHtml += html + "<style>{}</style>".format(USER_STYLES) 
+    else:
+        review_hack.review_html_scripts += html + "<style>{}</style>".format(USER_STYLES)
+
+    if MODE == "dictionary":
+        did = mw.col.decks.byName(DECK)
+        if not did:
+            mw.col.decks.id(DECK)
+        mid = mw.col.models.byName(NOTETYPE)
+        if not mid:
+            addModel(mw.col)
+        if not did or not mid:
+            mw.reset()
 
 
 # Hooks
 
 addHook("profileLoaded", setupAddon)
-Reviewer._initWeb = wrap(Reviewer._initWeb, addJavascriptObjects, "after")
-Reviewer._revHtml += html + "<style>{}</style>".format(USER_STYLES) 
+
